31c71e6010bbc9eaa47c85f3a9327853460fc18f
raj.gopal <raj.gopal@sankheki.com>
2016-10-16 23:28:46 +0530

    new spinner added

--- public/javascripts/issue.js
+++ public/javascripts/issue.js
@@ -1,21 +1,29 @@
-var IssueApp = angular.module('IssueApp',[]);
-IssueApp.controller('IssueAppCtrl',function($scope, issueFactory) {
+var IssueApp = angular.module('IssueApp',['angular-ladda']);
+IssueApp.controller('IssueAppCtrl',function($scope, issueFactory, $timeout) {
 
     $scope.link = "https://github.com/Shippable/support/issues";
     $scope.issueData;
+    $scope.countryList = ["INDIA","USA","UK"];
 
     /**
      * function which sends the link address entered by the user and in response gets the details of the issue
      */
     $scope.getData = function() {
         var data = {};
-        $scope.loading = true; //enabling loading
-        data["link"] = $scope.link;
-        issueFactory.getData(data).success(function(response) {
-            console.log(response);
-            $scope.issueData = response;
-            $scope.loading = false; //disabling loading
-        });
+        $scope.loading = true; // start loading
+        $timeout(function() {
+            data["link"] = $scope.link;
+            issueFactory.getData(data).success(function(response) {
+                console.log(response);
+                $scope.issueData = response;
+                $scope.loading = false; //disabling loading
+            });
+            $scope.loading = false; // stop loading
+        }, 2000);
+    };
+
+    $scope.displayResult = function() {
+
     };
     $scope.getData();
 });
